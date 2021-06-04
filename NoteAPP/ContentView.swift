79d71453acfd2fb381988b5c95a5b909704d3f75
Yevgeny Levin <devlev1980@gmail.com>
2021-06-04 14:06:48 +0300

    Initial Commit

---
+++ NoteAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoteAPP
+//
+//  Created by Yevgeny Levin on 04/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
