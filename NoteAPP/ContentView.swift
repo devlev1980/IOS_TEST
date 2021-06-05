265b1a0197eb89fb6fb8ceadd59ab276045a9355
Yevgeny Levin <devlev1980@gmail.com>
2021-06-05 20:39:40 +0300

    Add Angular Gradient

--- NoteAPP/ContentView.swift
+++ NoteAPP/ContentView.swift
@@ -12,10 +12,13 @@ struct ContentView: View {
         
       
         let colors = Gradient(colors: [.black,.blue,.green,.pink,.purple])
-            let gradient = RadialGradient(gradient: colors, center: .center, startRadius: 5, endRadius: 250)
+//            let gradient = RadialGradient(gradient: colors, center: .center, startRadius: 5, endRadius: 250)
+        
+        let gradient = AngularGradient(gradient: colors, center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/ )
             
             return Circle()
-                .fill(gradient)
+                .stroke(gradient,lineWidth: 10)
+//                .fill(gradient)
                 .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                                
       
